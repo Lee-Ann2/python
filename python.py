d40c91aadafed0839bce449f11d6112892971408
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-18 20:48:46 +0200

    That did not work, will try again

--- python.py
+++ python.py
@@ -1,12 +1,3 @@
 import random
 import math
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-colours = ['Red', 'Blue', 'Yellow', 'Green']
-superCards = ('Skip', 'Wild Card', 2, 'Reverse')
-
-def deckOfCards():
-    for number in numbers:
-        for colour in colours:
-            print(colour, end=' ')
-    number += 1
