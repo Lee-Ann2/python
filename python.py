cf6a3bdef04a0ea94e637d1f574523faa4881bcf
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-18 21:06:53 +0200

    That did not work either. Still trying to loop and append the numbers to colours.

--- python.py
+++ python.py
@@ -1,9 +1,17 @@
 import random
 import math
-
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#stack = []
+"""numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 colours = ['Red', 'Blue', 'Yellow', 'Green']
 superCards = ['Skip', 'Wild Card', '+2', 'Reverse']
 
 def deckOfCards():
+    stack = []
     for colour in colours:
+        for number in numbers:
+            cards = '{} {}'.format(colour, number)
+            stack.append(cards)
+print(stack)
+return(stack)
+"""
+
