247b3bcd3d9cf40cedf4ad0e598ae70022c5538d
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-18 20:55:12 +0200

    Trying to loop through the cards and numbers inside the function

--- python.py
+++ python.py
@@ -1,3 +1,9 @@
 import random
 import math
 
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+colours = ['Red', 'Blue', 'Yellow', 'Green']
+superCards = ['Skip', 'Wild Card', '+2', 'Reverse']
+
+def deckOfCards():
+    for colour in colours:
