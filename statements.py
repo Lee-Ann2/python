5dd828e226035f60b463036556f7ac82319b0e91
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-24 22:44:05 +0200

    Another if statement practice

--- statements.py
+++ statements.py
@@ -14,3 +14,20 @@ if alien_colour == 'green':
 #    print('Wow')
 else:
     print('yeah')
+
+'''
+5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and
+write an if-else chain.
+•	 If the alien’s color is green, print a statement that the player just earned
+5 points for shooting the alien.
+•	 If the alien’s color isn’t green, print a statement that the player just earned
+10 points.
+•	 Write one version of this program that runs the if block and another that
+runs the else block.
+
+'''
+alien_colour = 'red'
+if alien_colour == 'green':
+    print('You have earned 5 points')
+else: 
+    print('You earned 10 points.')
