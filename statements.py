c9a0ae79c9c675a889948f516f0958132ada6f3c
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-24 23:05:10 +0200

    Getting an error in line 97 about asssigning a literal

--- statements.py
+++ statements.py
@@ -82,3 +82,21 @@ elif age <= 65:
 else:
     print('Whatever man')
 
+
+'''
+5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of
+independent if statements that check for certain fruits in your list.
+•	 Make a list of your three favorite fruits and call it favorite_fruits.
+•	 Write five if statements. Each should check whether a certain kind of fruit
+is in your list. If the fruit is in your list, the if block should print a statement,
+such as You really like bananas!
+
+'''
+fruits = ['apples', 'banana', 'kiwi', 'orange', 'pear', 'raspberry']
+
+for 'apple' in fruits:
+    print('You really like apples')
+for 'banana' in fruits:
+    print('You really like bananas')
+for 'kiwi' in fruits:
+    print('You really like kiwis')
