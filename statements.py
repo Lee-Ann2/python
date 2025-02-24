05aaff4351ff474c02394f009a4026ef61eb6293
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-24 22:59:21 +0200

    More on statements

--- statements.py
+++ statements.py
@@ -51,3 +51,34 @@ elif alien_colour == 'yellow':
     print('You earned 25 points')
 else:
     print('Tf did you pick?')
+
+
+'''
+5-6. Stages of Life: Write an if-elif-else chain that determines a person’s
+stage of life. Set a value for the variable age, and then:
+•	 If the person is less than 2 years old, print a message that the person is
+a baby.
+•	 If the person is at least 2 years old but less than 4, print a message that
+the person is a toddler.
+•	 If the person is at least 4 years old but less than 13, print a message that
+the person is a kid.
+•	 If the person is at least 13 years old but less than 20, print a message that
+the person is a teenager.
+•	 If the person is at least 20 years old but less than 65, print a message that
+the person is an adult.
+•	 If the person is age 65 or older, print a message that the person is an
+elder.
+
+'''
+age = 26 
+if age <= 4:
+    print('You are a toddler')
+elif age <= 13:
+    print('You are a kid')
+elif age <= 20:
+    print('You are a teenager')
+elif age <= 65:
+    print('You are an adult')
+else:
+    print('Whatever man')
+
