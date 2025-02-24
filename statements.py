8e61a1383e0eb803580c9d6095b8180512c8c2d9
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-24 22:48:02 +0200

    Working with if/ elif/ else statements.

--- statements.py
+++ statements.py
@@ -31,3 +31,23 @@ if alien_colour == 'green':
     print('You have earned 5 points')
 else: 
     print('You earned 10 points.')
+
+'''
+5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elifelse chain.
+•	 If the alien is green, print a message that the player earned 5 points.
+•	 If the alien is yellow, print a message that the player earned 10 points.
+•	 If the alien is red, print a message that the player earned 15 points.
+•	 Write three versions of this program, making sure each message is printed
+for the appropriate color alien.
+
+'''
+
+alien_colour == 'red'
+if alien_colour == 'green':
+    print('You earned 5 points')
+elif alien_colour == 'red':
+    print('You earned 15 points')
+elif alien_colour == 'yellow':
+    print('You earned 25 points')
+else:
+    print('Tf did you pick?')
