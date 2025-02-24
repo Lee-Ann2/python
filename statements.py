059d8a39b4fe3191378d9ab5594e8161027109ff
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-24 23:50:01 +0200

    looping through and copying a list. Also using the if statements.

--- statements.py
+++ statements.py
@@ -136,3 +136,28 @@ if users == []:
     print('More users')
 else:
     print('Ok we see you')
+
+
+'''
+5-10. Checking Usernames: Do the following to create a program that simulates
+how websites ensure that everyone has a unique username.
+•	 Make a list of five or more usernames called .
+•	 Make another list of five usernames called new_users. Make sure one or
+two of the new usernames are also in the current_users list.
+•	 Loop through the new_users list to see if each new username has already
+been used. If it has, print a message that thecurrent_users person will need to enter a
+new username. If a username has not been used, print a message saying
+that the username is available.
+•	 Make sure your comparison is case insensitive. If 'John' has been used,
+'JOHN' should not be accepted. (To do this, you’ll need to make a copy of
+current_users containing the lowercase versions of all existing users.)
+
+'''
+current_users = ['alpha', 'bravo', 'charlie', 'john', 'jane', 'doe']
+new_users = ['bravo', 'John', 'Jane', 'admin', 'Name']
+for new_user in new_users:
+    if new_users == current_users:
+        print('User exist')
+    else:
+        print('User is available')
+    
