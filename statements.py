60ebc0d46865072dc9aafd23b61c1436002f6c65
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-24 23:25:46 +0200

    Normal if statement

--- statements.py
+++ statements.py
@@ -120,3 +120,18 @@ for username in usernames:
         print('Hello, Admin mfethu.')
     else:
         print(f'Hello, {username}')
+
+'''
+5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is
+not empty.
+•	 If the list is empty, print the message We need to find some users!
+•	 Remove all of the usernames from your list, and make sure the correct
+message is printed.
+
+'''
+
+users = ['admin', 'other', 'user', 'and', 'another', 'one']
+if users == []:
+    print('More users')
+else:
+    print('Ok we see you')
